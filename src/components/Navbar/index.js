3651da7b50159941918569d22dda90b36c48bb12
BuenoIT <101655079+BuenoIT@users.noreply.github.com>
2022-10-31 19:39:40 -0400

    project starting

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Bueno IT</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About me</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="background">Background</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="portfolio">Porfolio</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/contactme">Contact me</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
