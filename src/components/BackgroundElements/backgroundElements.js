5c34995eb0960a61c5ca893ed6c764cad656ae84
BuenoIT <101655079+BuenoIT@users.noreply.github.com>
2022-11-20 14:36:06 -0500

    Timeline Page development is complete

--- src/components/BackgroundElements/backgroundElements.js
+++ src/components/BackgroundElements/backgroundElements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link as LinkR } from 'react-router-dom';
 
 export const BackgroundWrapper = styled.div`
   background: linear-gradient(168deg, #d8ece4 0%, #378e6a 100%);
@@ -7,6 +8,27 @@ export const BackgroundWrapper = styled.div`
 
 export const BackgroundH1 = styled.h1`
   font-size: 7em;
+  text-align: center;
+`;
+
+export const BackgroundBtn = styled(LinkR)`
+  text-align: center;
+  border-radius: 50px;
+  background: #42c920;
+  padding: 10px 22px;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #313131;
+  }
+`;
+export const BackgroundP1 = styled.p`
   text-align: center;
 `;
 
@@ -33,4 +55,12 @@ export const BackgroundImg = styled.img`
   width: 80px;
   height: 80px;
   margin: 5px;
+`;
+
+export const BackgroundFigureWrap = styled.figure``;
+
+export const BackgroundFigCaption = styled.figcaption`
+  text-align: center;
+  font-size: 13px;
+  font-weight: lighter;
 `;
--- src/components/BackgroundElements/timelineData.js
+++ src/components/BackgroundElements/timelineData.js
@@ -5,7 +5,7 @@ export let timelineElements = [
     location: 'Waterloo, Canada',
     decription:
       'The program emphasizes problem-solving skills using the latest versions of Microsoft, Java, and open-source software to develop business and web applications. Object-oriented analysis, design, agile methodologies, and programming techniques are mastered and applied throughout the program.',
-    achievements: 'GPA: Current 3.98',
+    achievements: [{ name: 'GPA: Current 3.98' }],
     date: 'Jan 2022 - Apr 2023 (In progress)',
     company: 'Conestoga College',
     icon: 'school',
@@ -18,7 +18,6 @@ export let timelineElements = [
     location: 'Sao Paulo, Brazil',
     decription:
       'Management of API integration projects between banks to provide credit card payment services, loans and payment of due accounts',
-    achievements: '',
     date: 'Apr 2021 - Aug 2021',
     company: 'Dock',
     icon: 'work',
@@ -29,7 +28,7 @@ export let timelineElements = [
     id: 3,
     title: 'MBA in Project Management',
     location: 'Sao Paulo, Brazil',
-    achievements: 'GPA: 3.96',
+    achievements: [{ name: 'GPA: 3.96' }],
     decription:
       'MBA in Project Management from USP, integrating planning, execution and control through agile project management. MBA aligned with PMBOK, the PMI (Project Management Institute) best practice guide.',
     date: 'Oct 2019 - Jul 2021',
@@ -39,38 +38,143 @@ export let timelineElements = [
     alt: 'USP Esalq Logo',
   },
   {
-    id: 3,
-    title: 'Project Manager',
-    location: 'Sao Paulo, BR',
-    decription:
-      'Management of API integration projects between banks to provide credit card payment services, loans and payment of due accounts',
-    date: 'Apr 21 - Aug 21',
-    company: 'Dock Technology',
-    icon: 'work',
-    companyLogo: 'https://imgbox.com/26HxIROG',
-    alt: 'Dock Logo',
-  },
-  {
     id: 4,
     title: 'Project Manager',
-    location: 'Lojas Riachuelo S/A',
+    location: 'Sao Paulo, Brazil',
+    achievements: [
+      {
+        name: '1. A budget of $1M was approved for developing the website to increase sales by 20%',
+      },
+      {
+        name: '2. Managing various teams and tasks within a six-month timeframe',
+      },
+    ],
     decription:
-      'Project management for building eCommerce for Carters company in Brazil. Website availability with front-end development and backend integration with legacy systems',
-    date: 'Oct 20 - Apr 21',
-    company: 'Walmart',
+      '- Transition Carters e-commerce platform from a third party to the Riachuelo management and legacy system. To support their growth in Brazil, we built a customized e-commerce platform. - Controlling and managing the work of different departments, such as commercial, marketing, IT, logistics, and operations',
+    date: 'Apr 2021 - Aug 2021',
+    company: 'Riachuelo',
     icon: 'work',
-    companyLogo: '',
-    alt: 'Lojas Riachuelo S/A',
+    companyLogo: require('../../images/logo-riachuelo.png'),
+    alt: 'Riachuelo Store Logo',
   },
   {
     id: 5,
     title: 'Project Manager',
-    location: 'Sao Paulo',
+    location: 'Sao Paulo, Brazil',
     decription:
-      '- Management of business intelligence projects for AB-InBev companies.\\n - Portfolio management of 30 simultaneous projects, with resource, cost, and delivery quality management.',
-    date: 'August 18',
-    company: 'Walmart',
+      '- Management of business intelligence projects for AB-InBev companies - Portfolio management of 30 simultaneous projects, with resource, cost, and delivery quality management',
+    date: 'Aug 2019 - Mar 2020',
+    company: 'Keyrus',
     icon: 'work',
-    companyLogo: '',
+    companyLogo: require('../../images/keyrus-logo.webp'),
+    alt: 'Keyrus Logo',
+  },
+  {
+    id: 6,
+    title: 'Administrative Coordinator',
+    location: 'Vancouver, Canada',
+    decription:
+      "- Experience in critical analysis and development of an improvement plan for the company's logistics processes, for example, a new pallet rack structure to better adapt the products stored in the distribution center and a new dynamic of shift changes aimed at the iteration of employees and knowledge transfer - Ability to consolidate accounts payable for drivers responsible for deliveries in the company's internal system and experience in managing people with regard to monitoring and developing employees in order to manage a career plan and personal development of employees;",
+    date: 'Apr 2018 - Oct 2018',
+    company: 'Day & Ross Freight',
+    icon: 'work',
+    companyLogo: require('../../images/day-n-ross-logo.png'),
+    alt: 'Day & Ross Freight Logo',
+  },
+  {
+    id: 7,
+    title: 'Project Manager',
+    location: 'Sao Paulo, Brazil',
+    decription:
+      "- Experience in the implementation of the company's new e-commerce platform together with third-party companies aimed at restructuring the company and adapting processes to increase sales and brand visibility in Brazil; - Ability to allocate and develop teams for this project in the areas of organizational change management (GMO), processes, and projects; - Coordination of agendas and meetings for project decision-making between project stakeholders and those indirectly involved; - Ability to consolidate project documents in a shared folder and SharePoint to structure the materials according to the PMBOK guide;",
+    date: 'Sep 2016 - Sep 2017',
+    company: 'CVS Health',
+    icon: 'work',
+    companyLogo: require('../../images/CVS-Health-logo.jpg'),
+    alt: 'CVS Health Logo',
+    achievements: [
+      {
+        name: '1. Implementation of a redesigned eCommerce platform that increased sales by 30%;',
+      },
+      {
+        name: '2. Throughout the 1-year initiative, the project was managed without impacting release dates or scope;',
+      },
+      {
+        name: '3. A hybrid project management methodology was used: Waterfall for project management and Agile/Scrum for development;',
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "Bachelor's degreee in Chemical Production Engineer",
+    location: 'Sao Paulo, Brazil',
+    achievements: [{ name: 'GPA: 3.00' }],
+    decription:
+      'Bachelor of Chemical Production Engineering completed in Brazil.',
+    date: 'Jan 2012 - Mar 2017',
+    company: 'Oswaldo Cruz Colleges',
+    icon: 'school',
+    companyLogo: require('../../images/oswaldo-cruz-logo.jpg'),
+    alt: 'Oswaldo Cruz Colleges Logo',
+  },
+  {
+    id: 9,
+    title: 'Project Analyst',
+    location: 'Sao Paulo, Brazil',
+    achievements: [
+      {
+        name: '1. A budget of $1M was approved to develop projects in the logistics department to increase productivity and reduce costs;',
+      },
+      {
+        name: '2. Supervision of six people on the IT project development team;',
+      },
+    ],
+    decription:
+      '- Ability to execute projects in the area of continuous improvement with a focus on reducing costs and increasing productivity for the logistics networks of Recife, Minas Gerais, and Sao Paulo; - Ability to identify improvements and search for solutions in the market to transform ideas into projects within the company; - Experience in updating and generating status reports to company executives as well as Kick-Off presentations to project stakeholders involving the area of continuous improvement;',
+    date: 'Apr 2015 - Apr 2016',
+    company: 'Walmart.com',
+    icon: 'work',
+    companyLogo: require('../../images/walmart-logo.jpg'),
+    alt: 'Walmart.com Logo',
+  },
+  {
+    id: 10,
+    title: 'Junior Project Analyst',
+    location: 'Sao Paulo, Brazil',
+    achievements: [
+      {
+        name: '1. Management of the transition from a waterfall-based development to an iterative and agile/scrum-based development process;',
+      },
+      {
+        name: 'Development of KPIs for portfolio budget management ($60M CAPEX, $30M OPEX);',
+      },
+    ],
+    decription:
+      '- Ability to calculate costs and generate executive reports for development projects with separation of Capex and Opex costs to visualize the investments made in the company; - Experience in resource management for development projects aiming at quality in project deliveries and monitoring of project evolution; - Assistance in customizing the Clarity PPM tool for resource management (Timesheet) and project portfolio management as well as investments;',
+    date: 'Apr 2014 - Apr 2015',
+    company: 'Walmart.com',
+    icon: 'work',
+    companyLogo: require('../../images/walmart-logo.jpg'),
+    alt: 'Walmart.com Logo',
+  },
+  {
+    id: 11,
+    title: 'Project Trainee',
+    location: 'Sao Paulo, Brazil',
+    achievements: [
+      {
+        name: '1. Every week, maintain status updates of approximately 50 projects at the same time;',
+      },
+      {
+        name: '2. Produce English reports for executives (Directors and Vice Presidents);',
+      },
+    ],
+    decription:
+      '- Keep project portfolio status report up-to-date by following deliverables weekly with product owners; - Work with project management tools such as JIRA, Confluence, Clarity PPM; - Work with administrative tools such as Microsoft Excel, PowerPoint, Access, and Visio;',
+    date: 'Apr 2013 - Apr 2014',
+    company: 'Walmart.com',
+    icon: 'work',
+    companyLogo: require('../../images/walmart-logo.jpg'),
+    alt: 'Walmart.com Logo',
   },
 ];
